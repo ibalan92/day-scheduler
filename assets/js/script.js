745ce2a02f0ca3c98e6787d305bbb047d8a7da17
ibalan92 <ibalan92@gmail.com>
2023-11-23 19:55:05 +0000

    Added the current date

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var today = dayjs();
+var remainder = parseInt(today.format("DD"));
+if(remainder % 10 ===1){
+    $("#currentDay").text(today.format("dddd, MMMM D")+"st");
+}
+else if (remainder % 10 ===2){
+    $("#currentDay").text(today.format("dddd, MMMM D")+"nd");
+}
+else if (remainder % 10 ===3){
+    $("#currentDay").text(today.format("dddd, MMMM D")+"rd");
+}
+else {
+    $("#currentDay").text(today.format("dddd, MMMM D")+"th");
+}
