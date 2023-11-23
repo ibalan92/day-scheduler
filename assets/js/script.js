b02740fb3672f65062402046f44b5c3c2fafe48e
ibalan92 <ibalan92@gmail.com>
2023-11-23 22:13:52 +0000

    Handled the rest of the cases one by one

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,49 @@ function currentTime() {
     else if(hour<9){
         $("textarea").attr("class", "future");
     }
+    else if(hour<10){
+        $("#textarea1").attr("class", "present");
+        $("#textarea2,#textarea3,#textarea4,#textarea5,#textarea6,#textarea7,#textarea8,#textarea9").attr("class","future");
+    }
+    else if(hour<11){
+        $("#textarea1").attr("class", "past");
+        $("#textarea2").attr("class", "present");
+        $("#textarea3,#textarea4,#textarea5,#textarea6,#textarea7,#textarea8,#textarea9").attr("class","future");
+    }
+    else if(hour<12){
+        $("#textarea1,#textarea2").attr("class", "past");
+        $("#textarea3").attr("class", "present");
+        $("#textarea4,#textarea5,#textarea6,#textarea7,#textarea8,#textarea9").attr("class","future");
+    }
+    else if(hour<13){
+        $("#textarea1,#textarea2,#textarea3").attr("class", "past");
+        $("#textarea4").attr("class", "present");
+        $("#textarea5,#textarea6,#textarea7,#textarea8,#textarea9").attr("class","future");
+    }
+    else if(hour<14){
+        $("#textarea1,#textarea2,#textarea3,#textarea4").attr("class", "past");
+        $("#textarea5").attr("class", "present");
+        $("#textarea6,#textarea7,#textarea8,#textarea9").attr("class","future");
+    }
+    else if(hour<15){
+        $("#textarea1,#textarea2,#textarea3,#textarea4,#textarea5").attr("class", "past");
+        $("#textarea6").attr("class", "present");
+        $("#textarea7,#textarea8,#textarea9").attr("class","future");
+    }
+    else if(hour<16){
+        $("#textarea1,#textarea2,#textarea3,#textarea4,#textarea5,#textarea6").attr("class", "past");
+        $("#textarea7").attr("class", "present");
+        $("#textarea8,#textarea9").attr("class","future");
+    }
+    else if(hour<17){
+        $("#textarea1,#textarea2,#textarea3,#textarea4,#textarea5,#textarea6,#textarea7").attr("class", "past");
+        $("#textarea8").attr("class", "present");
+        $("#textarea9").attr("class","future");
+    }
+    else if(hour<18){
+        $("#textarea1,#textarea2,#textarea3,#textarea4,#textarea5,#textarea6,#textarea7,#textarea8").attr("class", "past");
+        $("#textarea9").attr("class","present");
+    }
   }
 
 setInterval(currentTime, 1000);
