fe49fb14c485281921c1f12bdfff88c6dfed263b
ibalan92 <ibalan92@gmail.com>
2023-11-24 10:53:04 +0000

    Handled refresh page scenario, checking the local storage and redisplay if exists

--- assets/js/script.js
+++ assets/js/script.js
@@ -69,6 +69,10 @@ function currentTime() {
 
 setInterval(currentTime, 1000);
 
+var task1 = window.localStorage.getItem("task1")
+if (task1 !==""){
+    $('#textarea1').val(task1);
+}
 
 $('#button1').on('click', function() {
     var textareaEl = $(this).siblings('.col').find('textarea');
@@ -76,11 +80,21 @@ $('#button1').on('click', function() {
     textareaEl.val(window.localStorage.getItem("task1"));
 });
 
+var task2 = window.localStorage.getItem("task2")
+if (task2 !==""){
+    $('#textarea2').val(task2);
+}
+
 $('#button2').on('click', function() {
     var textareaEl = $(this).siblings('.col').find('textarea');
     window.localStorage.setItem("task2", textareaEl.val());
     textareaEl.val(window.localStorage.getItem("task2"));
 });
+
+var task3 = window.localStorage.getItem("task3")
+if (task3 !==""){
+    $('#textarea3').val(task3);
+}
 
 $('#button3').on('click', function() {
     var textareaEl = $(this).siblings('.col').find('textarea');
@@ -88,11 +102,21 @@ $('#button3').on('click', function() {
     textareaEl.val(window.localStorage.getItem("task3"));
 });
 
+var task4 = window.localStorage.getItem("task4")
+if (task4 !==""){
+    $('#textarea4').val(task4);
+}
+
 $('#button4').on('click', function() {
     var textareaEl = $(this).siblings('.col').find('textarea');
     window.localStorage.setItem("task4", textareaEl.val());
     textareaEl.val(window.localStorage.getItem("task4"));
 });
+
+var task5 = window.localStorage.getItem("task5")
+if (task5 !==""){
+    $('#textarea5').val(task5);
+}
 
 $('#button5').on('click', function() {
     var textareaEl = $(this).siblings('.col').find('textarea');
@@ -100,23 +124,41 @@ $('#button5').on('click', function() {
     textareaEl.val(window.localStorage.getItem("task5"));
 });
 
+var task6 = window.localStorage.getItem("task6")
+if (task6 !==""){
+    $('#textarea6').val(task6);
+}
+
 $('#button6').on('click', function() {
     var textareaEl = $(this).siblings('.col').find('textarea');
     window.localStorage.setItem("task6", textareaEl.val());
     textareaEl.val(window.localStorage.getItem("task6"));
 });
 
+var task7 = window.localStorage.getItem("task7")
+if (task7 !==""){
+    $('#textarea7').val(task7);
+}
 $('#button7').on('click', function() {
     var textareaEl = $(this).siblings('.col').find('textarea');
     window.localStorage.setItem("task7", textareaEl.val());
     textareaEl.val(window.localStorage.getItem("task7"));
 });
 
+var task8 = window.localStorage.getItem("task8")
+if (task8 !==""){
+    $('#textarea8').val(task8);
+}
 $('#button8').on('click', function() {
     var textareaEl = $(this).siblings('.col').find('textarea');
     window.localStorage.setItem("task8", textareaEl.val());
     textareaEl.val(window.localStorage.getItem("task8"));
 });
+
+var task9 = window.localStorage.getItem("task9")
+if (task9 !==""){
+    $('#textarea9').val(task9);
+}
 
 $('#button9').on('click', function() {
     var textareaEl = $(this).siblings('.col').find('textarea');
